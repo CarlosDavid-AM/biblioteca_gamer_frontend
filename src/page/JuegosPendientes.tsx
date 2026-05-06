66bb5cdb69778eda207446415e702b070a5918a5
Carlos A Mendoza <apolayamendozacarlos@gmail.com>
2026-05-05 21:41:54 -0500

    Se agregan los cards con sus estados a JuegosPendientes y terminados.

--- src/page/JuegosPendientes.tsx
+++ src/page/JuegosPendientes.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { NavBar } from "../components/NavBar";
+import type { Juego } from "../interface/TypesJuego";
+import useFetchJuegos from "../hooks/useFetchJuegos";
+import JuegosCard from "../components/JuegosCard";
 
 const JuegosPendientes = () => {
+  const { getJuegos } = useFetchJuegos();
+  const [juegos, setJuegos] = useState<Juego[]>([]);
+
+  useEffect(() => {
+    const obtenerJuegos = async () => {
+      const juegos = await getJuegos("pendientes");
+      setJuegos(juegos);
+    };
+
+    obtenerJuegos();
+  }, [getJuegos]);
+
   return (
     <div>
       <NavBar />
-      JuegosPendientes
+      <JuegosCard juegos={juegos} />
     </div>
   );
 };
--- src/page/JuegosTerminados.tsx
+++ src/page/JuegosTerminados.tsx
@@ -1,10 +1,26 @@
 import { NavBar } from "../components/NavBar";
+import { useEffect, useState } from "react";
+import type { Juego } from "../interface/TypesJuego";
+import useFetchJuegos from "../hooks/useFetchJuegos";
+import JuegosCard from "../components/JuegosCard";
 
 const JuegosTerminados = () => {
+  const { getJuegos } = useFetchJuegos();
+  const [juegos, setJuegos] = useState<Juego[]>([]);
+
+  useEffect(() => {
+    const obtenerJuegos = async () => {
+      const juegos = await getJuegos("terminados");
+      setJuegos(juegos);
+    };
+
+    obtenerJuegos();
+  }, [getJuegos]);
+
   return (
     <div>
       <NavBar />
-      JuegoTerminado
+      <JuegosCard juegos={juegos} />
     </div>
   );
 };
