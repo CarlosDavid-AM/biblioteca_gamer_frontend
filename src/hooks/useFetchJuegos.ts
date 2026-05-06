2b98a5f61b8ab752895e77a43824daa0770a571e
Carlos A Mendoza <apolayamendozacarlos@gmail.com>
2026-05-05 21:12:19 -0500

    Se modifica el hook para recivir un parametro de estado

--- src/hooks/useFetchJuegos.ts
+++ src/hooks/useFetchJuegos.ts
@@ -2,8 +2,12 @@ import { useCallback } from "react";
 import { type Juego } from "../interface/TypesJuego";
 
 const useFetchJuegos = () => {
-  const getJuegos = useCallback(async (): Promise<Juego[]> => {
-    const datos = await fetch("http://localhost:8080/api/juegos");
+  const getJuegos = useCallback(async (estado?: string): Promise<Juego[]> => {
+    const url = estado
+      ? `http://localhost:8080/api/juegos/${estado}`
+      : "http://localhost:8080/api/juegos";
+
+    const datos = await fetch(url);
     const juegos = await datos.json();
 
     return juegos;
